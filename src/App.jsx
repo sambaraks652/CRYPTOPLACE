ac78c568e8dbb7b1253c1004700effa90d0ee046
sambaraks652 <sambaraks652@gmail.com>
2025-07-29 20:30:32 +0300

    just added home page and styling to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Coin from "./pages/Coin/Coin.jsx";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coin/:coinId" element={<Coin />} />
+      </Routes>
     </div>
   );
 };
